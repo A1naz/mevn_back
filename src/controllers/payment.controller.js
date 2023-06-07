055efee063434e9d2a03befae0e1ff43cf652200
A1naz <105604176+A1naz@users.noreply.github.com>
2023-06-07 17:54:23 +0500

    Update payment.controller.js

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -2,10 +2,10 @@ const QiwiBillPaymentsAPI = require('@qiwi/bill-payments-node-js-sdk')
 const { Order } = require('../model')
 
 const SECRET_KEY =
-  'eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6ImF5MTU3dS0wMCIsInVzZXJfaWQiOiI3OTYwMzgxNzYwNyIsInNlY3JldCI6ImU2NDhmNGRmNjAzNzhhMmQ3NzkzZjIzZTljZDg5MjkwNThlYzQ3N2I5NjgxZjVlMmQwOGVkMjI3Mzg4ZThmZmUifX0='
+  '='
 const qiwiApi = new QiwiBillPaymentsAPI(SECRET_KEY)
 const publicKey =
-  '48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iPpaxBk9oX6mpnoNH7qpvHnT1d5MGN2EF1SwUXvXAKq3EtNeeqPfdMEhoZ5FMzysFryN8NqmN35P2ApDFHYBY9LGd3eD7n5arY383UQXz8K'
+  ''
 
 const createPaymentIntent = async (
   { body: { fullname, address, phone, email, products } },
